80104d87ab0064510055f450e32b431ca5771e7c
Dakshjain1 <54345634+Dakshjain1@users.noreply.github.com>
2020-05-24 13:48:24 +0530

    This is Machine Learning file

--- train.py
+++ train.py
@@ -1,36 +1,94 @@
 # libraries
 import pandas as pd
-from keras.optimizers import Adam
-from keras.layers import Dense
-from keras.models import Sequential
-from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+import joblib
 
-#reading the dataset
-dataset = pd.read_csv('/code/wines.csv')
+# reading datasets
+train_data = pd.read_csv('/code/train.csv')
+test_data = pd.read_csv('/code/titanic_test.csv')
+validation_data = pd.read_csv('/code/titanic_validation.csv')
 
-# pre-processing & cleaning the dataset
-y = dataset['Class']
-y_cat = pd.get_dummies(y)
-x = dataset.drop('Class', axis=1)
+# pre-processing and cleaning on the training dataset
+age = train_data['Age']
 
-# spliting the dataset
-x_train, x_test, y_train, y_test = train_test_split(x,y_cat,test_size=0.1,random_state=20)
+def imputation(cols):
+    age = cols[0]
+    Pclass = cols[1]
+    if pd.isnull(age):
+        if Pclass == 1:
+            return 38
+        elif Pclass == 2:
+            return 30
+        elif Pclass == 3:
+            return 25
+        else:
+            return 30
+    else:
+        return age
 
-#creating the model
-model = Sequential()
-model.add(Dense(units=64, input_shape=(13,), activation='relu'))
-model.add(Dense(units=3, activation='softmax'))
-model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer=Adam())
-model.fit(x_train,y_train, validation_data=(x_test,y_test), epochs=20, verbose=0)
+train_data['Age'] = train_data[['Age', 'Pclass']].apply(imputation, axis=1)
 
-#calculating accuracy
-accuracy = model.evaluate(x_test, y_test, verbose=0)
-accuracy = accuracy[1]*100
+train_data['Age']
+train_data.drop('Cabin', axis=1, inplace=True)
+y_train = train_data['Survived']
+x_train = train_data[['Age','Pclass', 'Sex',  'SibSp','Parch','Embarked']]
+age = train_data['Age']
 
-# storing accuracy
+Pclass = train_data['Pclass']
+Pclass = pd.get_dummies(Pclass, drop_first=True, prefix='Pclass')
+
+Sibsp = train_data['SibSp']
+Sibsp = pd.get_dummies(Sibsp, drop_first=True, prefix='Sibsp')
+
+Parch = train_data['Parch']
+Parch = pd.get_dummies(Parch, drop_first=True, prefix='Parch')
+
+sex = train_data['Sex']
+sex = pd.get_dummies(sex, drop_first=True, prefix='Sex')
+
+embarked = train_data['Embarked']
+embarked = pd.get_dummies(embarked, drop_first=True, prefix='Embarked')
+
+x_train = pd.concat([age,Pclass, Sibsp, Parch, sex, embarked], axis=1)
+x_test = test_data[['Age','Pclass', 'Sex',  'SibSp','Parch','Embarked']]
+
+# applying the same function & pre-processing on test data
+test_data['Age'] = test_data[['Age', 'Pclass']].apply(imputation, axis=1)
+age = test_data['Age']
+
+Pclass = test_data['Pclass']
+Pclass = pd.get_dummies(Pclass, drop_first=True, prefix='Pclass')
+
+Sibsp = test_data['SibSp']
+Sibsp = pd.get_dummies(Sibsp, drop_first=True, prefix='Sibsp')
+
+Parch = test_data['Parch']
+Parch = pd.get_dummies(Parch, drop_first=True, prefix='Parch')
+
+sex = test_data['Sex']
+sex = pd.get_dummies(sex, drop_first=True, prefix='Sex')
+
+embarked = test_data['Embarked']
+embarked = pd.get_dummies(embarked, drop_first=True, prefix='Embarked')
+
+x_test = pd.concat([age,Pclass, Sibsp, Parch, sex, embarked], axis=1)
+x_test.drop('Parch_9', inplace=True, axis=1)
+y_test = validation_data['Survived']
+
+# model creation
+model = LogisticRegression()
+model.fit(x_train,y_train)
+y_pred = model.predict(x_test)
+
+# accuracy
+report = classification_report(y_test,y_pred)
+report_new = report.split('\n')[-2]
+accuracy = float(report_new.split()[2])*100
+print("Your Accuracy is {}".format(accuracy))
+
+joblib.dump(model,'/code/Logistic_regression.pk1')
+
 import os
-os.system("sudo touch /code/accuracy.txt")
+os.system("touch /code/accuracy.txt")
 os.system("echo {} > /code/accuracy.txt".format(accuracy))
-
-# saving the model to send to the client
-model.save('/code/multiclassNN.h5')
